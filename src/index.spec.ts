8a9d87520ef938953c1722eb8a8c6f8c2c7aef6a
shocker <fmju96@gmail.com>
2023-05-29 20:13:47 -0400

    Library structure, added tests and docs

---
+++ src/index.spec.ts
@@ -0,0 +1,5 @@
+import { testFunction } from './index';
+
+test('two plus two is four', () => {
+  expect(testFunction()).toBe('testing');
+});
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export function testFunction() {
+  const a = 1;
+  return 'testing';
+}
