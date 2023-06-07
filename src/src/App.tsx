ef55e657866354bd35b6f1057769993155c3c3cb
shocker <fmju96@gmail.com>
2023-06-07 13:41:26 -0400

    Docs build

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -1,5 +1,5 @@
 import { useDeferredValue, useEffect, useMemo, useState } from 'react';
-import { schemesFromColor, setCSSRules } from 'material-color-scheme';
+import { schemesFromColor, setCSSRules } from '../../dist';
 import './App.css';
 
 const customColorsInitialValue = [
